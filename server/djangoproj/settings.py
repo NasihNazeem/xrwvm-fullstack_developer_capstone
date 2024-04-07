22873039b8c8099adc950f6bbbe9e6acf5586e10
NasihNazeem <nasihnazeem@gmail.com>
2024-04-07 05:28:39 -0400

    restapis fixed 16

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://nasihnazeem-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    '''https://nasihnazeem-8000.theiadockernext-1-labs-prod-theiak8s-4-
+    tor01.proxy.cognitiveclass.ai'''
     ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://nasihnazeem-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    '''https://nasihnazeem-8000.theiadockernext-1-labs-prod-theiak8s-4-
+    tor01.proxy.cognitiveclass.ai'''
     ]
 
 REST_FRAMEWORK = {
