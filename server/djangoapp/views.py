7aec48ed3657a6f2abcf92091accedbf5c2b85fc
NasihNazeem <nasihnazeem@gmail.com>
2024-04-07 04:55:48 -0400

    restapis fixed 7

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -93,17 +93,21 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append(
+            {
+                "CarModel": car_model.name,
+                "CarMake": car_model.car_make.name
+            })
     return JsonResponse({"CarModels": cars})
 
 
-def get_dealerships(request, state="All"):
+def get_dealerships(request, state = "All"):
     if (state == "All"):
         endpoint = "/fetchDealers"
     else:
         endpoint = "/fetchDealers/"+ state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status": 200,"dealers": dealerships})
+    return JsonResponse({"status": 200, "dealers": dealerships})
 
 
 def get_dealer_reviews(request, dealer_id):
@@ -129,10 +133,9 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
             return JsonResponse({"status":200})
         except Exception as err:
             print(f"Unexpected {err=}, {type(err)=}")
