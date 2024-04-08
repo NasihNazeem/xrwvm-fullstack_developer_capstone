4b20ebe29a47424013d9dbe0083ab19cdcad4ebc
NasihNazeem <nasihnazeem@gmail.com>
2024-04-08 17:50:10 -0400

    Revert "populate fixed"
    This reverts commit 81052c70b4b2091b40aa286f079077147059bf96.

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -9,27 +9,25 @@ load_dotenv()
 backend_url = os.getenv(
     'backend_url', default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url', default="http://localhost:5050/")
+    'sentiment_analyzer_url',
+    default="http://localhost:5050/")
 
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
-
-
-def get_request(endpoint, **kwargs):
-    params = ""
-    if (kwargs):
-        for key, value in kwargs.items():
+def get_request(endpoint, **kwargs): 
+    params = "" 
+    if(kwargs): 
+        for key, value in kwargs.items(): 
             params = params + key + "=" + value + "&"
 
     request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
-    try:
+    try: 
         response = requests.get(request_url)
         return response.json()
         # Call get method of requests library with URL and parameters
-    except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
+    except: 
         print("Network exception occurred")
         # If any error occurs
 
@@ -40,11 +38,11 @@ def get_request(endpoint, **kwargs):
 
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
-    try:
+    try: 
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception as err:
+    except Exception as err: 
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
@@ -54,10 +52,9 @@ def analyze_review_sentiments(text):
 
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
-    try:
+    try: 
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
+    except: 
         print("Network exception occurred")
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -9,7 +9,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 
 # Get an instance of a logger
@@ -37,9 +37,8 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
-
-
 def logout_request(request):
+    
     logout(request)
     return JsonResponse({"userName": ""})
 
@@ -62,56 +61,45 @@ def registration(request):
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
-        print("Network exception occurred")
+    except:
+        logger.debug("{} is new user".format(username))
         # If not, simply log this is a new user
 
     # If it is a new user
-    if not username_exist:
+    if not username_exist: 
         # Create user in auth_user table
-        user = User.objects.create_user(
-                username=username,
-                first_name=first_name,
-                last_name=last_name,
-                password=password,
-                email=email)
+        user = User.objects.create_user(username=username, 
+            first_name=first_name, last_name=last_name, 
+            password=password, email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else:
+    else: 
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
-
-def get_cars(request):
+def get_cars(request): 
     count = CarMake.objects.filter().count()
     print(count)
-    if (count == 0):
+    if(count == 0): 
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append(
-            {
-                "CarModel": car_model.name,
-                "CarMake": car_model.car_make.name
-            })
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
-
-def get_dealerships(request, state="All"):
-    if (state == "All"):
+def get_dealerships(request, state="All"): 
+    if(state == "All"): 
         endpoint = "/fetchDealers"
-    else:
-        endpoint = "/fetchDealers/" + state
+    else: 
+        endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status": 200, "dealers": dealerships})
-
+    return JsonResponse({"status": 200,"dealers": dealerships})
 
 def get_dealer_reviews(request, dealer_id):
-    if (dealer_id):
+    if(dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
@@ -122,28 +110,21 @@ def get_dealer_reviews(request, dealer_id):
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
-
 def get_dealer_details(request, dealer_id):
-    if (dealer_id):
+    if(dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status": 200, "dealer": dealership})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
-
 def add_review(request):
-    if (request.user.is_anonymous is False):
-        # data = json.loads(request.body)
+    if(request.user.is_anonymous == False):
+        data = json.loads(request.body)
         try:
-            return JsonResponse({"status": 200})
-        except Exception as err:
-            print(f"Unexpected {err=}, {type(err)=}")
-            print("Network exception occurred")
-            return JsonResponse(
-                {
-                    "status": 401,
-                    "message": "Error in posting review"
-                })
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status": 401, "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
