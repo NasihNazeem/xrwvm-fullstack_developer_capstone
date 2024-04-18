9bd848b7ac68ef8764fafe75eeb5fb243900bcf8
NasihNazeem <nasihnazeem@gmail.com>
2024-04-18 13:09:50 -0400

    updated env vars and re-organized djangoproj routes

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -21,6 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('djangoapp/', include('djangoapp.urls')),
+    path('', TemplateView.as_view(template_name="Home.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
@@ -30,6 +32,4 @@ urlpatterns = [
         template_name="index.html"
         )),
     path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-    path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
